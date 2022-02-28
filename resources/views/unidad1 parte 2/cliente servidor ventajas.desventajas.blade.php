f4131c07636d22542587b832543e20b1b7b66c18
unknown <fotoprueba8@gmail.com>
2022-02-27 22:03:19 -0600

    carga gaby

--- resources/views/unidad1 parte 2/cliente servidor ventajas.desventajas.blade.php
+++ resources/views/unidad1 parte 2/cliente servidor ventajas.desventajas.blade.php
@@ -5,16 +5,22 @@
       <li><a href="#">Home</a></li>
       <li><a href="#">Apuntes</a></li>
       <li><a href="#">Parcial 1</a></li>
-      <li><a href="#">Arquitectura cliente servidor</a></li>
+      <li><a href="#">Cliente servidor Ventajas y desventajas</a></li>
     </ul>
 @endsection
 
 @section('contenido')
 <div class="content"> 
       <!-- ################################################################################################ -->
-      <h1>titulo</h1>
-      <h2>subtitulo si es necesario si no se puede borrrar :)</h2>
-      <p>aqui la info(si no quieres que todo este en un parrafo ya sabes pones signo mayor p signo menor</p>
+      <h1>CLIENTE SERVIDOR VENTAJAS Y DESVENTAJAS<h1>
+      
+      <p>Centralización del control: Los accesos, recursos y la integridad de los datos son controlados por el servidor de forma que un programa cliente defectuoso o no autorizado no pueda dañar el sistema. Esta centralización también facilita la tarea de poner al día datos u otros recursos (mejor que en las redes P2P).</p>
+      <p>Escalabilidad: Se puede aumentar la capacidad de clientes y servidores por separado. Cualquier elemento puede ser aumentado (o mejorado) en cualquier momento, o se pueden añadir nuevos nodos a la red (clientes y/o servidores).</p>
+      <p>Fácil mantenimiento: Al estar distribuidas las funciones y responsabilidades entre varios ordenadores independientes, es posible reemplazar, reparar, actualizar, o incluso trasladar un servidor, mientras que sus clientes no se verán afectados por ese cambio (o se afectarán mínimamente). Esta independencia de los cambios también se conoce como encapsulación.</p>
+      <p>Existen tecnologías, suficientemente desarrolladas, diseñadas para el paradigma de C/S que aseguran la seguridad en las transacciones, la amigabilidad del interfaz, y la facilidad de empleo.</p>
+      <h2>DESVENTAJAS</h2>
+      <p>La congestión del tráfico ha sido siempre un problema en el paradigma de C/S. Cuando una gran cantidad de clientes envían peticiones simultaneas al mismo servidor, puede ser que cause muchos problemas para éste (a mayor número de clientes, más problemas para el servidor). Al contrario, en las redes P2P como cada nodo en la red hace también de servidor, cuantos más nodos hay, mejor es el ancho de banda que se tiene.</p>
+      <p>El paradigma de C/S clásico no tiene la robustez de una redP2P. Cuando un servidor está caído, las peticiones de los clientes no pueden ser satisfechas. En la mayor parte de redes P2P, los recursos están generalmente distribuidos en varios nodos de la red. Aunque algunos salgan o abandonen la descarga; otros pueden todavía acabar de descargar consiguiendo datos del resto de los nodos en la red.</p>
       <!-- ################################################################################################ -->
     </div>
 @endsection
--- resources/views/unidad1 parte 2/computo en la nube.blade.php
+++ resources/views/unidad1 parte 2/computo en la nube.blade.php
@@ -5,16 +5,28 @@
       <li><a href="#">Home</a></li>
       <li><a href="#">Apuntes</a></li>
       <li><a href="#">Parcial 1</a></li>
-      <li><a href="#">Arquitectura cliente servidor</a></li>
+      <li><a href="#">COMPUTO DE LA NUBE</a></li>
     </ul>
 @endsection
 
 @section('contenido')
 <div class="content"> 
       <!-- ################################################################################################ -->
-      <h1>titulo</h1>
-      <h2>subtitulo si es necesario si no se puede borrrar :)</h2>
-      <p>aqui la info(si no quieres que todo este en un parrafo ya sabes pones signo mayor p signo menor</p>
+      <h1>COMPUTO DE LA NUBE</h1>
+      <h2>¿Que es la computacion en la nube?</h2>
+      <p> La computación en la nube es la entrega de recursos informáticos, incluidos el almacenamiento, la potencia de procesamiento, las bases de datos, las redes, los análisis, la inteligencia artificial y las aplicaciones de software, a través de Internet (la nube). </p>
+      <h2>CARACTERISTICAS</h2>
+      <p>Antes de la llegada de la computación en la nube, las organizaciones compraban y mantenían una infraestructura local de TI. Aunque el ahorro de costes impulsó gran parte del cambio inicial a la nube, muchas organizaciones opinan que las infraestructuras de nube pública, privada o híbrida ofrecen numerosas ventajas.</p>
+      <h3>FUNDAMENTOS</h3>
+      <p>Cuando una empresa decide "migrar a la nube", significa que traslada su infraestructura de TI fuera del entorno local, a un centro de datos mantenido por un proveedor de Cloud Computing (como Oracle). Un proveedor de nube líder de la industria tiene la responsabilidad de gestionar la infraestructura de TI del cliente, integrar las aplicaciones y desarrollar nuevas capacidades y funcionalidades para seguir el ritmo de las demandas del mercado.</p>
+      <h3>TIPOS DE COMPUTOS DE NUBES</h3>
+      <p>NUBE PRIVADA</p>
+      <p>La nube privada es de uso exclusivo de una organización. Se puede alojar en la ubicación de la organización o en el centro de datos del proveedor de la nube. La nube privada proporciona un nivel más alto de seguridad y control.</p>
+      <p>NUBE HIBRIDA</p>
+      <p>Como su nombre indica, una nube híbrida es una combinación de nubes públicas y privadas. En general, el cliente aloja sus aplicaciones críticas en sus propios servidores para mayor seguridad y control, y las aplicaciones secundarias se almacenan en el entorno de un proveedor de nube.</p>
+      <p>MULTINUBE</p>
+      <p>La principal diferencia entre nube híbrida y multinube es el uso de varios dispositivos de Cloud Computing y almacenamiento en una sola arquitectura. Hay que centrarse en añadir algunas palabras clave en el cuerpo.</p>
+      
       <!-- ################################################################################################ -->
     </div>
 @endsection
